aca1a7a9a5362a4f2904a9c225f8beb41e2645be
Stefano Sanguinetti <stefano.sanguinetti@alumnos.ucn.cl>
2024-09-05 08:10:34 -0400

    Http and mongo configured

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 const connectDB = (url) =>
-    mongoose.connect(url).then(() => console.log('Database connected'));
+    mongoose.connect(url).then(() => console.log('Conectado a la base datos'));
 
 export default connectDB;
